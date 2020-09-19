a5c3acacc400d583bb8e0df63f67bf179e8f5e77
zhang19980107 <943248247@qq.com>
2020-09-20 01:32:21 +0800

    01 学习python爬虫

---
+++ index.py
@@ -0,0 +1,18 @@
+import urllib.request
+import ssl
+ssl._create_default_https_context=ssl._create_unverified_context;
+
+def load_data():
+    url = "http://www.baidu.com/"
+    # get请求
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    str_data = data.decode()
+    print(str_data)
+    with open("src/baidu.html", "w", encoding="utf-8")as f:
+        f.write(str_data)
+
+
+load_data()
+
+
